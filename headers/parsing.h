45da5ce2044d1dbc6200a73cf02ec28f805c74a2
Gyuyong Choi <dragon0170@gmail.com>
2016-10-28 19:11:26 +0900

    Revert "no message"
    This reverts commit 00f12f3a8bfc7390f14a4297b8b7ebfcbf7d30e0.

--- headers/parsing.h
+++ headers/parsing.h
@@ -1,23 +1,4 @@
 #ifndef PARSING_H
 #define PARSING_H
-#include<QString>
-#include<QQueue>
-#include<QStringList>
-
-class MdString{
-
-public:
-    int getDepth();
-    void setDepth(int);
-    int getText();
-    void setText(QString);
-
-private:
-    int depth;
-    QString text;
-
-};
-
-int getQqueue(QString&,QQueue&);
 
 #endif // PARSING_H

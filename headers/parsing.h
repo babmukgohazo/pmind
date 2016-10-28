00f12f3a8bfc7390f14a4297b8b7ebfcbf7d30e0
Gyuyong Choi <dragon0170@gmail.com>
2016-10-28 19:10:36 +0900

    no message

--- headers/parsing.h
+++ headers/parsing.h
@@ -1,4 +1,23 @@
 #ifndef PARSING_H
 #define PARSING_H
+#include<QString>
+#include<QQueue>
+#include<QStringList>
+
+class MdString{
+
+public:
+    int getDepth();
+    void setDepth(int);
+    int getText();
+    void setText(QString);
+
+private:
+    int depth;
+    QString text;
+
+};
+
+int getQqueue(QString&,QQueue&);
 
 #endif // PARSING_H
